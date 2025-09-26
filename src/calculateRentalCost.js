8e9a7960e4db38775d4ea348198b13c88c0cc0bc
Yurii Drutsul <druculurij@gmail.com>
2025-09-26 12:03:07 +0300

    message

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,7 +8,7 @@ function calculateRentalCost(days) {
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUND = 20;
+  const SHORT_TERM_DISCOUNT = 20;
   const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
@@ -16,7 +16,7 @@ function calculateRentalCost(days) {
   }
 
   if (days >= SHORT_TERM) {
-    return basePrice - SHORT_TERM_DISCOUND;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   return basePrice;
